e92504bee5329c9f9e4b0f97886b2eb8ac894b17
Zachary Lehmann <zlehmann@gmail.com>
2019-02-03 12:58:09 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :authors, only: [:show, :index] do
-    resources :posts, only: [:show, :index, :new]
+    resources :posts, only: [:show, :index, :new, :edit]
   end
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
